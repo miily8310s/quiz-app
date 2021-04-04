f8ff38e86d08cef201988c154eafa76646a28055
miily8310s <rillakerika@gmail.com>
2021-04-04 15:28:37 +0900

    add fetch api

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,5 +1,34 @@
 import React from 'react';
+interface Props {
+  question: string;
+  answers: string[];
+  callback: any;
+  userAnswer: any;
+  questionNo: number;
+  totalQuestions: number;
+}
 
-const QuestionCard = () => <div>Question Card</div>;
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNo,
+  totalQuestions,
+}) => (
+  <>
+    <p className='number'>
+      Question: {questionNo} / {totalQuestions}
+    </p>
+    <p dangerouslySetInnerHTML={{ __html: question }}></p>
+    {answers.map((answer) => (
+      <div>
+        <button disabled={userAnswer} onClick={callback}>
+          <span dangerouslySetInnerHTML={{ __html: answer }}></span>
+        </button>
+      </div>
+    ))}
+  </>
+);
 
 export default QuestionCard;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { QuestionState, fetchQuizQuestions, Difficulty } from '../api';
+
+import QuestionCard from '../components/QuestionCard';
+
+const TOTAL_QUESTIONS = 10;
 
 export default function Home() {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY));
+
+  const startTrivia = async () => {};
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+  const nextQuestion = () => {};
   return (
-    <>
-      <div className='App'>Quiz</div>
-    </>
+    <div className='App'>
+      <h1>REACT QUIZ</h1>
+      <button className='start' onClick={startTrivia}>
+        Start
+      </button>
+      <p className='score'>Score:</p>
+      <p>Loading Questions ...</p>
+      {/* <QuestionCard
+        questionNo={number + 1}
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
+      /> */}
+      <button className='next' onClick={nextQuestion}>
+        Next Question
+      </button>
+    </div>
   );
 }
